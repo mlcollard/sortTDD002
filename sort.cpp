f50ea7318e01cd632056790ad8ec83ad2eaa0c9e
Michael L. Collard, Ph.D <collard@uakron.edu>
2019-03-11 15:23:40 -0400

    Sort a reversed vector of two elements

--- sort.cpp
+++ sort.cpp
@@ -9,4 +9,10 @@
 // sort the entire vector in ascending order
 void sort(std::vector<int>& v) {
 
+	if (v.size() <= 1)
+		return;
+
+	int t = v[0];
+	v[0] = v[1];
+	v[1] = t;
 }
--- sort_t.cpp
+++ sort_t.cpp
@@ -33,5 +33,21 @@ int main() {
         assert(v[0] == 1);
     }
 
+    {
+        std::vector<int> v;
+        v.push_back(2);
+        v.push_back(1);
+
+        assert(!v.empty());
+        assert(v.size() == 2);
+        assert(v[0] == 2);
+        assert(v[1] == 1);
+        sort(v);
+        assert(!v.empty());
+        assert(v.size() == 2);
+        assert(v[0] == 1);
+        assert(v[1] == 2);
+    }
+
     return 0;
 }
