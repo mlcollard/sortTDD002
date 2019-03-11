de5c0d1f1013e860cb8151fb0ed78e39e85237a3
Michael L. Collard, Ph.D <collard@uakron.edu>
2019-03-11 15:25:59 -0400

    Sort a vector of two elements

--- sort.cpp
+++ sort.cpp
@@ -12,7 +12,9 @@ void sort(std::vector<int>& v) {
 	if (v.size() <= 1)
 		return;
 
-	int t = v[0];
-	v[0] = v[1];
-	v[1] = t;
+	if (v[0] > v[1]) {
+		int t = v[0];
+		v[0] = v[1];
+		v[1] = t;
+	}
 }
